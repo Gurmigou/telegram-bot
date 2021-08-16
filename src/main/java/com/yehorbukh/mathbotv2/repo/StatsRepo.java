33abc20fda2a51014993538fcdb5c9aa6633f18a
Gurmigou <43072571+Gurmigou@users.noreply.github.com>
2021-08-17 00:33:26 +0300

    Updated math bot 5

--- src/main/java/com/yehorbukh/mathbotv2/repo/StatsRepo.java
+++ src/main/java/com/yehorbukh/mathbotv2/repo/StatsRepo.java
@@ -10,27 +10,27 @@ import java.util.List;
 import java.util.Optional;
 
 public interface StatsRepo extends CrudRepository<UserStats, Integer> {
-    @Query(value = "SELECT name, words FROM stats, user WHERE chat_id =: chat_id and stats.user_id = user.user_id ORDER BY words desc limit 12",
+    @Query(value = "SELECT name, words FROM stats, user WHERE chat_id=:chat_id and stats.user_id = user.user_id ORDER BY words desc limit 12",
            nativeQuery = true)
     List<Object[]> wordsTop(@Param("chat_id") long chatId);
 
-    @Query(value = "SELECT name, stickers FROM stats, user WHERE chat_id =: chat_id and stats.user_id = user.user_id ORDER BY stickers desc limit 12",
+    @Query(value = "SELECT name, stickers FROM stats, user WHERE chat_id=:chat_id and stats.user_id = user.user_id ORDER BY stickers desc limit 12",
             nativeQuery = true)
     List<Object[]> stickerTop(@Param("chat_id") long chatId);
 
-    @Query(value = "SELECT name, gifs FROM stats, user WHERE chat_id =: chat_id and stats.user_id = user.user_id ORDER BY gifs desc limit 12",
+    @Query(value = "SELECT name, gifs FROM stats, user WHERE chat_id=:chat_id and stats.user_id = user.user_id ORDER BY gifs desc limit 12",
             nativeQuery = true)
     List<Object[]> gifsTop(@Param("chat_id") long chatId);
 
-    @Query(value = "SELECT name, swear_words FROM stats, user WHERE chat_id =: chat_id and stats.user_id = user.user_id ORDER BY swear_words desc limit 12",
+    @Query(value = "SELECT name, swear_words FROM stats, user WHERE chat_id=:chat_id and stats.user_id = user.user_id ORDER BY swear_words desc limit 12",
             nativeQuery = true)
     List<Object[]> swearWordsTop(@Param("chat_id") long chatId);
 
-    @Query(value = "SELECT name, video_notes FROM stats, user WHERE chat_id =: chat_id and stats.user_id = user.user_id ORDER BY video_notes desc limit 12",
+    @Query(value = "SELECT name, video_notes FROM stats, user WHERE chat_id=:chat_id and stats.user_id = user.user_id ORDER BY video_notes desc limit 12",
             nativeQuery = true)
     List<Object[]> videoNotesTop(@Param("chat_id") long chatId);
 
-    @Query(value = "SELECT name, voices FROM stats, user WHERE chat_id =: chat_id and stats.user_id = user.user_id ORDER BY voices desc limit 12",
+    @Query(value = "SELECT name, voices FROM stats, user WHERE chat_id=:chat_id and stats.user_id = user.user_id ORDER BY voices desc limit 12",
             nativeQuery = true)
     List<Object[]> voicesTop(@Param("chat_id") long chatId);
 
