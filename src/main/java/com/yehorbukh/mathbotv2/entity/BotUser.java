67f6926a5e185ef89cdd0d21094f6deae6395564
Gurmigou <egor555x6@gmail.com>
2021-09-16 20:41:08 +0300

    stucture modified

--- src/main/java/com/yehorbukh/mathbotv2/entity/BotUser.java
+++ src/main/java/com/yehorbukh/mathbotv2/entity/BotUser.java
@@ -5,6 +5,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Entity
@@ -12,6 +14,7 @@ import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor
+@NotNull
 public class BotUser {
     @Id
     @Column(nullable = false)
